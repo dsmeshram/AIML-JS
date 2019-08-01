37bbac72b48a3fc7ecb5e363bb8d06867a5f8166
Damodhar Meshram <damodhar.meshram@hsmumsz.local>
2019-08-01 15:48:53 +0530

    add mean,mode,mediam and linearReagressition function added

---
+++ statstics.js
@@ -0,0 +1,89 @@
+var stat = function(){
+    this.stat = this;
+}
+
+stat.prototype.mean = function(list) {
+    //mean = sum(list)/n
+    if (Array.isArray(list)) {
+        var count = list.length;
+        var sum = list.reduce(sumArray, 0)
+        return sum / count;
+    } else {
+        console.log('data should be in array form. list = []')
+    }
+}
+
+function sumArray(total, num) {
+    return total + num;
+}
+
+stat.prototype.mode = function(list) {
+    if (Array.isArray(list)) {
+        var mf = 1;
+        var m = 0;
+        var item;
+        for (var i = 0; i < list.length; i++) {
+            for (var j = i; j < list.length; j++) {
+                if (list[i] == list[j])
+                    m++;
+                if (mf < m) {
+                    mf = m;
+                    item = list[i];
+                }
+            }
+            m = 0;
+        }
+        if (item != undefined) {
+            return item;
+        } else {
+            console.log('Mode not available');
+        }
+    } else {
+        console.log('Data should be in array form. list = []');
+    }
+}
+
+stat.prototype.mediam = function(list) {
+    if (Array.isArray(list)) {
+        if (list.length === 0) return 0;
+
+        list.sort(function (a, b) {
+            return a - b;
+        });
+
+        var half = Math.floor(list.length / 2);
+
+        if (list.length % 2)
+            return list[half];
+
+        return (list[half - 1] + list[half]) / 2.0;
+    } else {
+        console.log('Data should be in array form. list = []');
+    }
+}
+
+stat.prototype.linearReagressition= function(y,x){
+    //y = b0 + b1 * x
+    var lr = {};
+        var n = y.length;
+        var sum_x = 0;
+        var sum_y = 0;
+        var sum_xy = 0;
+        var sum_xx = 0;
+        var sum_yy = 0;
+
+        for (var i = 0; i < y.length; i++) {
+
+            sum_x += x[i];
+            sum_y += y[i];
+            sum_xy += (x[i]*y[i]);
+            sum_xx += (x[i]*x[i]);
+            sum_yy += (y[i]*y[i]);
+        } 
+
+        lr['slope'] = (n * sum_xy - sum_x * sum_y) / (n*sum_xx - sum_x * sum_x);
+        lr['intercept'] = (sum_y - lr.slope * sum_x)/n;
+        lr['r2'] = Math.pow((n*sum_xy - sum_x*sum_y)/Math.sqrt((n*sum_xx-sum_x*sum_x)*(n*sum_yy-sum_y*sum_y)),2);
+
+        return lr;
+}
